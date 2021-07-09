11ada91bc3d64726e490aad7fdf9fa429e1f6ea4
Chiaki <cmnicilover@gmail.com>
2021-07-09 07:13:19 -0700

    update about section, modal display settings

--- script.js
+++ script.js
@@ -21,38 +21,54 @@ function sortNav() {
 }
 
 //modal contact
-const viewBtn = document.querySelector(".contactBtn"),
-popup = document.querySelector(".popup"),
-close = popup.querySelector(".close");
-viewBtn.onclick = ()=>{
-	popup.classList.toggle("show");
-}
-close.onclick = ()=>{
-	viewBtn.click();
+var onOff = false;
+function toggle(){
+	var blur = document.getElementById('blur');
+	blur.classList.toggle('active');
+	var popup = document.getElementById('popup');
+	popup.classList.toggle('active');
+	if (!onOff) {
+		document.body.style.overflow = "hidden";
+		document.body.style.height = "100%";
+		onOff = true;
+	} else {
+		document.body.style.overflow = "auto";
+		document.body.style.height = "auto";
+		onOff = false;
+	}
+
 }
 
-//scroll to top
+// We select the element we want to target
+var target = document.querySelector("footer");
+
 var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
 var rootElement = document.documentElement
 
-function handleScroll() {
-	// Do something on scroll
-	var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-	if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
-		// Show button
-		scrollToTopBtn.classList.add("showBtn")
-	} else {
-		// Hide button
-		scrollToTopBtn.classList.remove("showBtn")
-	}
+// Next we want to create a function that will be called when that element is intersected
+function callback(entries, observer) {
+// The callback will return an array of entries, even if you are only observing a single item
+entries.forEach(entry => {
+if (entry.isIntersecting) {
+// Show button
+scrollToTopBtn.classList.add("showBtn")
+} else {
+// Hide button
+scrollToTopBtn.classList.remove("showBtn")
+}
+});
 }
 
 function scrollToTop() {
-	// Scroll to top logic
-	rootElement.scrollTo({
-		top: 0,
-		behavior: "smooth"
-	})
+rootElement.scrollTo({
+top: 0,
+behavior: "smooth"
+})
 }
-scrollToTopBtn.addEventListener("click", scrollToTop)
-document.addEventListener("scroll", handleScroll)
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+// Next we instantiate the observer with the function we created above. This takes an optional configuration
+// object that we will use in the other examples.
+let observer = new IntersectionObserver(callback);
+// Finally start observing the target element
+observer.observe(target);
